5d2527af08ae1709bcadf6ad47be86f49a3f244f
sudipta02 <sudipta.sundar95@gmail.com>
2023-07-04 17:14:12 +0530

    Login signup page

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Landing.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Landing.jsx
@@ -4,13 +4,17 @@ import React from "react";
 /// This is the landing page. You need to add a link to the login page here.
 /// Maybe also check from the backend if the user is already logged in and then show them a logout button
 /// Logging a user out is as simple as deleting the token from the local storage.
+
 function Landing() {
-    return <div>
-        <h1>Welcome to course selling website!</h1>
+    return (
+      <div className="landing-container">
+        <h1 style={{ marginBottom: "30px" }}>
+          Welcome to course selling website!
+        </h1>
         <a href="/register">Register</a>
-        <br/>
         <a href="/login">Login</a>
-    </div>
+      </div>
+    );
 }
 
 export default Landing;
--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Login.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Login.jsx
@@ -1,18 +1,114 @@
-import React from "react";
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
+const LOGIN_URL = "/auth";
 
-/// File is incomplete. You need to add input boxes to take input for users to login.
-function Login() {
-    const [email, setEmail] = React.useState("");
+const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
 
-    return <div>
-        <h1>Login to admin dashboard</h1>
-        <br/>
-        Email - <input type={"text"} onChange={e => setEmail(e.target.value)} />
-        <br/>
-        <button>Login</button>
-        <br/>
-        New here? <a href="/register">Register</a>
-    </div>
-}
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(JSON.stringify(response?.data));
+      //console.log(JSON.stringify(response));
+      const accessToken = response?.data?.accessToken;
+      const roles = response?.data?.roles;
+      setAuth({ user, pwd, roles, accessToken });
+      setUser("");
+      setPwd("");
+      setSuccess(true);
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus();
+    }
+  };
+
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#">Go to Home</a>
+          </p>
+        </section>
+      ) : (
+        <div className="login-container">
+          <section>
+            <p
+              ref={errRef}
+              className={errMsg ? "errmsg" : "offscreen"}
+              aria-live="assertive"
+            >
+              {errMsg}
+            </p>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+              />
+
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+              />
+              <button>Sign In</button>
+            </form>
+            <p>
+              Need an Account?
+              <br />
+              <span className="line">
+                {/*put router link here*/}
+                <a href="#">Sign Up</a>
+              </span>
+            </p>
+          </section>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Login;
