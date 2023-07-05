0566b59012df39ce0a1232addd4b15357f617235
sudipta02 <sudipta.sundar95@gmail.com>
2023-07-05 12:36:16 +0530

    course card

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/ShowCourses.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/ShowCourses.jsx
@@ -1,20 +1,49 @@
 import React from "react";
 
-function ShowCourses() {
-    const [courses, setCourses] = React.useState([]);
+const coursesTemp = [
+  {
+    title: "Title",
+    description: "Description",
+    price: 1000,
+    imageLink: "some_url",
+    published: true,
+  },
+];
 
-    // Add code to fetch courses from the server
-    // and set it in the courses state variable.
-    return <div>
-        <h1>Create Course Page</h1>
-        {courses.map(c => <Course title={c.title} />)}
+function ShowCourses() {
+  const [courses, setCourses] = React.useState(coursesTemp);
+
+  // Add code to fetch courses from the server
+  // and set it in the courses state variable.
+  return (
+    <div>
+      <h1>Create Course Page</h1>
+      {courses.map((c) => (
+        <Course
+          title={c.title}
+          description={c.description}
+          price={c.price}
+          imageLink={c.imageLink}
+          published={c.published}
+        />
+      ))}
     </div>
+  );
 }
 
 function Course(props) {
-    return <div>
-        <h1>{props.title}</h1>
+  const { title, description, price, imageLink, published } = props;
+  return (
+    <div className="course-card">
+      <div className="course-img"></div>
+      <div className="course-info">
+        <p>Title: {title}</p>
+        <p>Description: {description}</p>
+        <p>Price: {price}</p>
+        <p>Published: {String(published)}</p>
+      </div>
     </div>
+  );
 }
 
 export default ShowCourses;
